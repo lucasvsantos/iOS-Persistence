7aff10fb0f6738e6b2dd3f0d75c1fdaab684487b
Usuário Convidado <Guest@l2602micro39.local>
2018-08-24 19:25:03 -0300

    Initial Commit

---
+++ MyGames/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyGames
+//
+//  Created by Usuário Convidado on 24/08/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
